62ce5949a5f4f497e2ca7ca9288724f063e91a91
Unknown <geoff@crystalclearit.com.au>
2017-05-02 09:26:03 +1000

    1/5/17

---
+++ Assets/scripts/MouseCoord.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MouseCoord : MonoBehaviour {
+    private Text mouseCoord;
+	// Use this for initialization
+	void Start () {
+        mouseCoord = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      mouseCoord.text = "Mouse X: " + Input.mousePosition.x.ToString()+ " Mouse Y: " + Input.mousePosition.y.ToString();
+	}
+}
